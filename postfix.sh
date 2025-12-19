94efcccce6eeb561fa69df17625fe903de74d412
Jakob S. <jakicoll@users.noreply.github.com>
2025-12-19 12:11:45 +0100

    Remove obsolete postfix option smtp_use_tls
    The [docs](https://www.postfix.org/postconf.5.html#smtp_tls_security_level) for `smtp_tls_security_level` state that setting a value to it, "overrides the obsolete parameters smtp_use_tls".
    
    Since we set ``smtp_tls_security_level``, we can remove `smtp_use_tls`.

--- postfix.sh
+++ postfix.sh
@@ -18,7 +18,6 @@ postconf -e "smtp_host_lookup = native,dns"
 postconf -e "smtp_sasl_auth_enable = yes"
 postconf -e "smtp_sasl_password_maps = hash:/etc/postfix/sasl_password"
 postconf -e "smtp_sasl_security_options = noanonymous"
-postconf -e "smtp_use_tls = yes"
 postconf -e "smtp_tls_security_level = encrypt"
 postconf -e "header_size_limit = 4096000"
 echo "nameserver 1.1.1.1" > /var/spool/postfix/etc/resolv.conf
